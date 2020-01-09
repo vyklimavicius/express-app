e71a7dc1a3d80dea1252c875dc32137453b8d1f6
vyklimavicius <vyklimavicius@gmail.com>
2020-01-09 18:08:02 -0500

    prettier

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+// Middlewares
+app.use('/home', (req, resp, next) => {
+  console.log(`Request URL:  ${req.originalUrl}`);
+  console.log(`Request TYPE:  ${req.method}`);
+  resp.send('Hello from home!');
+  next();
+});
+// Routes
+// app.get('/', (req, res) => {
+//   res.status(200);
+//   res.send('This is my server');
+// });
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
-
-// Routes
-app.get("/", (req, res) => {
-  res.status(200);
-  res.send("This is my server");
+  console.log('Listening on port 3000');
 });
